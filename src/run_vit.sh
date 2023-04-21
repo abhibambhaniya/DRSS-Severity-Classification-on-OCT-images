9f8822f5c591d85bf5f51e4ac1608931b5ea53bf
Abhimanyu Bambhaniya <abambhaniya3@gatech.edu>
2023-04-21 15:46:42 -0400

    adding vit checkpoint

--- src/run_vit.sh
+++ src/run_vit.sh
@@ -1,4 +1,4 @@
 python train_DNN_model.py --model vit\
     --epoch 30 --batch_size 8 --lr 5e-3 --weight-decay 0.1 --lr-min 1e-4 --lr-warmup-epochs 5 --lr_scheduler True\
-    --amp --data_root /storage/home/hpaceice1/shared-classes/materials/ece8803fml \
-    --load_checkpoint /storage/home/hpaceice1/abambhaniya3/DRSS-Severity-Classification-on-OCT-images/VIT_model_checkpoints/vit20230413-214119_test_balanced_accuracy_.pt 
+    --amp --data_root /storage/home/hpaceice1/shared-classes/materials/ece8803fml 
+#     --load_checkpoint /storage/home/hpaceice1/abambhaniya3/DRSS-Severity-Classification-on-OCT-images/VIT_model_checkpoints/vit20230413-214119_test_balanced_accuracy_.pt 
